a5f6a59334818ab42509e145d7d20c4f90b422b7
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-02 09:59:13 -0300

    feat: update paper without border and background color

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -2,11 +2,10 @@ import { styled } from '@mui/material'
 import MuiPaper, { PaperProps } from '@mui/material/Paper'
 import { FC } from 'react'
 
-const StyledPaper = styled(MuiPaper)((props) => ({
-  '&': {
-    borderRadius: 4,
-    ...(props.elevation === 12 ? { boxShadow: '0px 2px 5px #00000005' } : {})
-  }
+const StyledPaper = styled(MuiPaper)(() => ({
+  backgroundColor: 'background.paper',
+  border: '0px solid #00000000',
+  boxShadow: '0px 0px 0px 0px #00000000'
 }))
 
 const Paper: FC<PaperProps> = (props) => {
