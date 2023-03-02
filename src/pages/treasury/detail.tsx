2eb68d2bb0ed7d550120cadeabcca686bd5d2ddd
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-02 09:59:53 -0300

    feat: add integration of cache

--- src/pages/treasury/detail.tsx
+++ src/pages/treasury/detail.tsx
@@ -2,7 +2,7 @@ import TimeLineScale from '@/src/components/Charts/TimeLineScale'
 import ContainerWrapper from '@/src/components/ContainerWrapper'
 import CustomTypography from '@/src/components/CustomTypography'
 import ErrorBoundaryWrapper from '@/src/components/ErrorBoundary/ErrorBoundaryWrapper'
-import { DataWarehouse } from '@/src/services/classes/dataWarehouse.class'
+import Cache from '@/src/services/classes/cache.class'
 import { getLastWeekDate, getTodayDate } from '@/src/utils'
 import {
   filterByRangeOfDates,
@@ -32,17 +32,15 @@ export default function Detail({ rows }: IDetailProps) {
 }
 
 export async function getServerSideProps() {
-  // Step 1: Create a BigQuery client
-  const dataWarehouse = DataWarehouse.getInstance()
+  // Step 1: Create a cache instance and obtain the data
+  const cache = Cache.getInstance()
+  const reportData = cache.getBalanceReports()
 
-  // Step 2: Query the data
-  const parsedRows = await dataWarehouse.getBalanceReports()
-
-  // Step 3: Filter the data by date range and DAO
+  // Step 2: Filter the data by date range and DAO
   const startDateTime = getLastWeekDate()
   const endDateTime = getTodayDate()
 
-  const parsedRowsFiltered = filterByRangeOfDates(parsedRows, startDateTime, endDateTime)
+  const parsedRowsFiltered = filterByRangeOfDates(reportData, startDateTime, endDateTime)
     .filter((item: any) => item['dao'] === 'Karpatkey')
     .sort((a: any, b: any) => {
       const aDate = a['kitche_date'].value
@@ -50,10 +48,10 @@ export async function getServerSideProps() {
       return new Date(aDate).getTime() - new Date(bDate).getTime()
     })
 
-  // Step 4: Reduce the data to a single object, with the date as key and the total balance as value
+  // Step 3: Reduce the data to a single object, with the date as key and the total balance as value
   const parsedRowsReduced = parsedRowsFiltered.reduce(reducerTotalBalancesByDate, {})
 
-  // Step 5: Convert the data to a format that can be used by the chart
+  // Step 4: Convert the data to a format that can be used by the chart
   const rows = mapDataToLineSeries(parsedRowsReduced)
 
   // Pass data to the page via props
