990aaa6706023e351b6e9303fe3b8e1ed0dee8c4
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-09 14:50:09 -0300

    refactor: move common server side props in the utils folder

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,3 +1,5 @@
+import Cache from '@/shared/services/classes/cache.class'
+import { filterByRangeOfDates } from '@/shared/utils/mappers'
 import { DateTime } from 'luxon'
 
 export const getLastWeekDate = () => {
@@ -6,4 +8,22 @@ export const getLastWeekDate = () => {
 
 export const getTodayDate = () => {
   return DateTime.local().endOf('day').toMillis()
+}
+
+export const getCommonServerSideProps = (): any[] => {
+  // Step 1: Create a cache instance and obtain the data
+  const cache = Cache.getInstance()
+  const reportData = cache.getBalanceReports()
+
+  // Step 2: Filter the data by date range and DAO
+  const startDateTime = getLastWeekDate()
+  const endDateTime = getTodayDate()
+
+  return filterByRangeOfDates(reportData, startDateTime, endDateTime)
+    .filter((item: any) => item['dao'] === 'Karpatkey')
+    .sort((a: any, b: any) => {
+      const aDate = a['kitche_date'].value
+      const bDate = b['kitche_date'].value
+      return DateTime.fromISO(aDate).toUTC().toMillis() - DateTime.fromISO(bDate).toUTC().toMillis()
+    })
 }
