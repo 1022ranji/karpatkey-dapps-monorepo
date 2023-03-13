2c1cf56453321da56b851734e21a0980e4300a4e
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-13 20:49:27 -0300

    fix: add deep

--- dapps/panic-button/src/components/ButtonActions/ButtonAction.tsx
+++ dapps/panic-button/src/components/ButtonActions/ButtonAction.tsx
@@ -50,7 +50,7 @@ export default function ButtonAction(props: IButtonActionProps) {
       return successMessage || ''
     }
     return ''
-  }, [error, data])
+  }, [error, successMessage, data])
 
   return (
     <Box
