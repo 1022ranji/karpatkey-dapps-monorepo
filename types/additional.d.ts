7e8bdc44a569ff05922b524f2203bcba33219f9d
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-20 09:30:30 -0300

    feat: add new types

--- types/additional.d.ts
+++ types/additional.d.ts
@@ -3,3 +3,28 @@ declare type Maybe<T> = T | null
 type ComponentProps = {
   children?: React.ReactNode
 }
+
+type NetworkId = 1 | 100
+
+enum NetworkIds {
+  ETHEREUM = 1,
+  GNOSIS = 100,
+}
+
+interface DAO {
+  address: string
+  name: string
+}
+
+interface Network {
+  DAOs: DAO[]
+}
+
+enum PeriodTime {
+  YEAR = 'year',
+  WEEK = 'week',
+  MONTH = 'month',
+  DAY = 'day'
+}
+
+type PeriodType = `${PeriodTime}`
