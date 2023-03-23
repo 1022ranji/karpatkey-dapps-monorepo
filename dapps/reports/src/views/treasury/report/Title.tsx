d285354f8a30a5e4e4ffe6c389adfc4f8efb07d3
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-23 12:35:13 -0300

    feat: add improvements to the title component

--- dapps/reports/src/views/treasury/report/Title.tsx
+++ dapps/reports/src/views/treasury/report/Title.tsx
@@ -1,11 +1,14 @@
 import { TTitleProps } from '@karpatkey-monorepo/reports/src/types'
 import CustomTypography from '@karpatkey-monorepo/shared/components/CustomTypography'
+import { getHumanDAO, isDefaultAddress } from '@karpatkey-monorepo/shared/utils'
 import * as React from 'react'
 
-const Title = ({ periodType, dao }: TTitleProps) => {
+const Title = ({ periodType, daoAddress, chainId }: TTitleProps) => {
+  const daoTitle =
+    daoAddress && !isDefaultAddress(daoAddress) ? getHumanDAO(daoAddress, chainId) : 'all the DAOs'
   return (
-    <CustomTypography color="textSecondary" variant="h4" textAlign="center">
-      A {periodType} in {dao} farms
+    <CustomTypography variant="h2" textAlign="center">
+      A {periodType} in {daoTitle} farms
     </CustomTypography>
   )
 }
