60cc717094b4e7151e6469525910bf2db7303f43
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-23 12:34:07 -0300

    feat: add console.log

--- shared/components/ErrorBoundary/ErrorBoundaryWrapper.tsx
+++ shared/components/ErrorBoundary/ErrorBoundaryWrapper.tsx
@@ -5,6 +5,7 @@ import ErrorBoundary from './ErrorBoundary'
 const ErrorBoundaryWrapper = (props: ComponentProps<any>): ReactElement => {
   const handleError = () => {
     // do something
+    console.log('TODO: error happened, use a log here')
   }
 
   return <ErrorBoundary handleError={handleError} {...props} />
