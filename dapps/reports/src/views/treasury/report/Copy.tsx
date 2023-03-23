927f8830f9bc28e741c15927afaf4301c143e545
Mariano Aguero <mariano.aguero@gmail.com>
2023-03-23 12:27:39 -0300

    feat: add copy component

--- dapps/reports/src/views/treasury/report/Copy.tsx
+++ dapps/reports/src/views/treasury/report/Copy.tsx
@@ -1,21 +1,26 @@
 import { TReportFilter } from '@karpatkey-monorepo/reports/src/types'
 import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import { Tooltip } from '@mui/material'
 import * as React from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const Copy = (props: TReportFilter) => {
-  const { periodType, dao, period } = props
-  const value = `/treasury/report?dao=${dao}&period=${period}&periodType=${periodType}`
+  const { chainId, periodType, daoAddress, period } = props
+  const value = React.useMemo(
+    () =>
+      `/treasury/report?chainId=${chainId}&daoAddress=${daoAddress}&period=${period}&periodType=${periodType}`,
+    [chainId, daoAddress, period, periodType]
+  )
   const onCopy = () => {
     window.open(value, '_blank')
   }
 
   return (
-    <>
-      <CopyToClipboard text={value} onCopy={onCopy}>
-        <OpenInNewIcon fontSize="large" sx={{ cursor: 'pointer' }} />
-      </CopyToClipboard>
-    </>
+    <CopyToClipboard text={value} onCopy={onCopy}>
+      <Tooltip title="Copy the filters and open it in another tab">
+        <OpenInNewIcon fontSize="medium" sx={{ cursor: 'pointer' }} />
+      </Tooltip>
+    </CopyToClipboard>
   )
 }
 
