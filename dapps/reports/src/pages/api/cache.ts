95aed497a03a6ac5038ee6f12b5f90645b6e22d7
Mariano Aguero <mariano.aguero@gmail.com>
2023-06-05 22:13:31 -0300

    feat: add type

--- dapps/reports/src/pages/api/cache.ts
+++ dapps/reports/src/pages/api/cache.ts
@@ -20,7 +20,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Status
 
       // Step 2: Query the data
       const cachePromises = ALLOWED_REPORTS.map(async (report) => {
-        const reportData = await dataWarehouse[report.reportName]()
+        const reportData = await dataWarehouse[report.reportName as Report]()
         cache.writeApi(report.reportName, reportData)
       })
       await Promise.all(cachePromises)
