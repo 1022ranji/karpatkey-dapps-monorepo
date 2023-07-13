afb0a49b7fc5c250b4c03de815e8dbd716fa621e
Mariano Aguero <mariano.aguero@gmail.com>
2023-07-13 15:59:45 -0300

    feat: remove zero values

--- dapps/reports/src/utils/format.ts
+++ dapps/reports/src/utils/format.ts
@@ -1,7 +1,7 @@
 import numbro from 'numbro'
 
 export const formatCurrency = (value: number, mantissa = 0) => {
-  if (value === 0) return '-'
+  if (value === 0 || (value < 0.5 && value > -0.5)) return '-'
 
   return numbro(value).formatCurrency({
     spaceSeparated: false,
