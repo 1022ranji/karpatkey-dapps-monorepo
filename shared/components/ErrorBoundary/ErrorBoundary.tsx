69d41a8703d287cb3ede05df2822d38f394d9795
Mariano Aguero <mariano.aguero@gmail.com>
2023-12-05 14:59:10 -0300

    feat: add november report

--- shared/components/ErrorBoundary/ErrorBoundary.tsx
+++ shared/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,16 +1,11 @@
 import dynamic from 'next/dynamic'
-import React, { ComponentProps, FC } from 'react'
+import React, { ComponentProps, ErrorInfo, FC } from 'react'
 import { ErrorBoundary as ErrorBoundaryComponent } from 'react-error-boundary'
 
 const ErrorFallback = dynamic(() => import('./ErrorFallback'), { ssr: false })
 
 interface ErrorBoundaryWrapperProps extends ComponentProps<any> {
-  handleError: (
-    error: Error,
-    info: {
-      componentStack: string
-    }
-  ) => void
+  handleError: (error: Error, info: ErrorInfo) => void
 }
 
 const ErrorBoundary: FC<ErrorBoundaryWrapperProps> = ({ handleError, children }) => (
