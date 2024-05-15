898f8e828f0582bc7de5b62e765a879ddc8a0565
Mariano Aguero <mariano.aguero@gmail.com>
2024-05-15 12:32:52 -0300

    fix: next.js issue

---
+++ commitlint.config.ts
@@ -0,0 +1,11 @@
+// @ts-check
+
+/* TODO: migrate from .js to .ts when
+ * https://github.com/TypeStrong/ts-node/issues/1007
+ * gets resolved.
+ */
+
+/** @typedef { import("@commitlint/types").UserConfig } */
+export default {
+  extends: ['@commitlint/config-conventional']
+}
